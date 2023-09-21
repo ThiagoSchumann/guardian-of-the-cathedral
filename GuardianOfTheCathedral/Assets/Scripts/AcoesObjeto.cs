4e05c1d6df05ca5b7c244e0792bccb7e2017e8b3
Thiago Artur Schumann <thiagoarturschumann@gmail.com>
2023-09-21 18:42:10 -0300

    Aprimoramentos na interação e visualização na segunda etapa do tutorial.
    - Adicionados cubos interativos que permitem ao jogador executar ações de arrastar e pegar.
    - Aprimoramento nas rotinas de visualização: agora contornos de diferentes cores identificam distintos tipos de objetos.
    - Otimização das rotinas de seleção de componentes: uma mensagem guia agora orienta o usuário sobre qual tecla pressionar para acionar comportamentos específicos (como pegar, arrastar e soltar).

---
+++ GuardianOfTheCathedral/Assets/Scripts/AcoesObjeto.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcoesObjeto : MonoBehaviour
+{
+    private IdentificarObjetos idObjetos;
+    private bool pegou = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        idObjetos = GetComponent<IdentificarObjetos>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && idObjetos.GetObjPegar() != null)
+        {
+            // print("Update -> Entrando em Pegar");
+            Pegar();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && idObjetos.GetObjArrastar() != null)
+        {
+            if (!pegou)
+            {
+                Arrastar();
+                print("Update -> Entrando em Arrastar");
+            }
+            else
+            {
+                Soltar();
+                print("Update -> Entrando em Soltar");
+            }
+            pegou = !pegou;
+        }
+    }
+
+    private void Pegar()
+    {
+        Destroy(idObjetos.GetObjPegar());
+        idObjetos.EsconderTexto();
+    }
+
+    private void Arrastar()
+    {
+        GameObject obj = idObjetos.GetObjArrastar();
+        obj.AddComponent<DragDrop>();
+        obj.GetComponent<DragDrop>().Ativar();
+        idObjetos.enabled = false;
+    }
+
+    private void Soltar()
+    {
+        GameObject obj = idObjetos.GetObjArrastar();
+        Destroy(obj.GetComponent<DragDrop>());
+        idObjetos.enabled = true;
+    }
+}
